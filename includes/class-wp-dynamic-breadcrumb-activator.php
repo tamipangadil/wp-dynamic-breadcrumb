782f2744155425baf88e88e2183d78aa4be3c8dc
Tami Pangadil <tamipangadil@gmail.com>
2015-06-07 10:29:08 +0800

    Commit Wordpress plugin boilerplate

---
+++ includes/class-wp-dynamic-breadcrumb-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.tamipangadil.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Dynamic_Breadcrumb
+ * @subpackage Wp_Dynamic_Breadcrumb/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Dynamic_Breadcrumb
+ * @subpackage Wp_Dynamic_Breadcrumb/includes
+ * @author     Tami Pangadil <tamipangadil@gmail.com>
+ */
+class Wp_Dynamic_Breadcrumb_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-dynamic-breadcrumb-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.tamipangadil.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Dynamic_Breadcrumb
+ * @subpackage Wp_Dynamic_Breadcrumb/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Dynamic_Breadcrumb
+ * @subpackage Wp_Dynamic_Breadcrumb/includes
+ * @author     Tami Pangadil <tamipangadil@gmail.com>
+ */
+class Wp_Dynamic_Breadcrumb_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
